35277cffbf480a80aca4efa51309299cde6af5f1
Vladimir Arger <8745@rambler.ru>
2023-05-19 12:50:15 +0400

    fix class name 'Main'

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,17 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double summCredit = 1_000_000;
         double creditPeriodYear = 2;
         int payForMonth = (int) service.calculate(summCredit, creditPeriodYear);
         System.out.println("Ежемесячный платеж, при сумме кредита " + summCredit + "руб. и сроке кредита " + creditPeriodYear + "года, составит " + payForMonth + "руб.");
-
-        System.out.println();
         System.out.println("Сумма платежа | Срок кредита(лет) | Ежемесячный платеж");
-
+        System.out.println();
         int payForMonthOneYear = (int) service.calculate(1_000_000, 1);
         System.out.println("1 000 000 | 1 |" + payForMonthOneYear);
-
         int payForMonthTwoYears = (int) service.calculate(1_000_000, 2);
-        System.out.println("1 000 000 | 2 |" + payForMonthTwoYears);
-
+        System.out.println("1 000 000 | 1 |" + payForMonthTwoYears);
         int payForMonthThreeYears = (int) service.calculate(1_000_000, 3);
-        System.out.println("1 000 000 | 3 |" + payForMonthThreeYears);
-
-        int payForMonthFourYears = (int) service.calculate(1_000_000, 4);
-        System.out.println("1 000 000 | 4 |" + payForMonthFourYears);
-
-        int payForMonthFiveYears = (int) service.calculate(1_000_000, 5);
-        System.out.println("1 000 000 | 5 |" + payForMonthFiveYears);
-
+        System.out.println("1 000 000 | 1 |" + payForMonthThreeYears);
     }
 }
-
